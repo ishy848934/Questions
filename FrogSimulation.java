295b34d51091a48afe6d3b00265ff9e7432a60a6
ishy848934 <ishamm2828@gmail.com>
2026-04-16 12:43:21 -0400

    yt
    njk

---
+++ FrogSimulation.java
@@ -0,0 +1,25 @@
+public class FrogSimulation{
+    private int goalDistance;
+    private int maxHops;
+    public FrogSimulation(int dist, int numHops){
+        goalDistance = dist;
+        maxHops = numHops;
+    }
+    private int hopDistance(){
+        int score = 0;
+
+        for( int i = 0;i < nums;i++){
+            if(score > maxHops){
+                score = maxHops;
+            }
+            else{
+                score != maxHops;
+            }
+        }
+            
+        }
+    }
+
+    public boolean simulate(){
+
+    }
